e4c6e6a2ad6f9246f36b0e78538959dc03074401
westleyj12 <westley.d.james@gmail.com>
2025-04-08 20:07:34 -0400

    Player and shapes added

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 
 import pygame
+from player import Player
 
-from constants import (SCREEN_HEIGHT,
-                       SCREEN_WIDTH,
-                       ASTEROID_KINDS,
-                       ASTEROID_MAX_RADIUS,
-                       ASTEROID_MIN_RADIUS,
-                       ASTEROID_SPAWN_RATE)
+from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        
         pygame.Surface.fill(screen,(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
     
